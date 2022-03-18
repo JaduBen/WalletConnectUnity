09364979df532dc4c60c5ccc60d21d0626c0c112
ben <b@jadu.ar>
2022-03-18 22:34:49 +0000

    rework package layout

---
+++ Runtime/Unity/Scripts/Models/WCMessageSigned.cs
@@ -0,0 +1,19 @@
+namespace WalletConnectSharp.Unity.Models
+{
+    public class WCMessageSigned
+    {
+        public MessageSigner Signer { get; private set; }
+
+        public string Message => Signer.messageToSign;
+
+        public string SignedMessage { get; private set; }
+        
+        //TODO Add an recovered address property with a cached variable
+
+        public WCMessageSigned(MessageSigner signer, string signedMessage)
+        {
+            Signer = signer;
+            SignedMessage = signedMessage;
+        }
+    }
+}
